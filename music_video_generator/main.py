4bab7da086e28f11cc46ebb74e59c9207710419f
Ruben Pozo <rubenpozomolina@gmail.com>
2025-02-01 13:27:12 +0100

    generating videos working

--- music_video_generator/main.py
+++ music_video_generator/main.py
@@ -31,46 +31,50 @@ def create_directory_if_not_exists(directory_path):
 
 def controls_to_images(
         controls_path, images_path, model_id,
-        prompt, negative_prompt="", lora=None
+        prompt, init_image,
+        negative_prompt="", lora=None
 ):
     create_directory_if_not_exists(images_path)
     files = get_all_files(controls_path)
-    openpose = OpenposeDetector.from_pretrained('lllyasviel/ControlNet')
     controlnet = ControlNetModel.from_pretrained(
-        "lllyasviel/sd-controlnet-openpose", torch_dtype=torch.float16
+        "lllyasviel/sd-controlnet-openpose",
+        torch_dtype=torch.float16
     )
     pipe = StableDiffusionControlNetPipeline.from_pretrained(
-        model_id,
-        controlnet=controlnet,
+        model_id, controlnet=controlnet,
         torch_dtype=torch.float16,
         safety_checker=None,
-        variant="fp16"
+        requires_safety_checker=False
     ).to("cuda")
+    torch.cuda.empty_cache()
+    pipe.enable_xformers_memory_efficient_attention()
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.enable_model_cpu_offload()
     if lora:
         lora_file_path = lora
         pipe.load_lora_weights(lora_file_path)
-    torch.manual_seed(2)
+    source_image = load_image(init_image)
     for file in files:
-        source_image = load_image(file)
-        control_image = openpose(source_image)
+        control_image = load_image(file)
         image = pipe(
-            prompt,
-            control_image,
+            prompt=prompt,
             negative_prompt=negative_prompt,
-            strength=0.4,
-            num_inference_steps=20,
+            init_image=source_image,
+            image=control_image,
+            num_inference_steps=30,
+            guidance_scale=20,
+            strength=0.7,
         ).images[0]
         image.save(images_path + os.sep + file.split("/")[-1])
-        # init_image = image
+        source_image = image
 
 
 def music_video_generator(
         input_video,
         output_folder,
         model_id,
-        prompt, negative_prompt="", lora=None
+        prompt, init_image,
+        negative_prompt="", lora=None
 ):
     video_to_frames(
         input_video,
@@ -88,6 +92,7 @@ def music_video_generator(
         images_folder,
         model_id,
         prompt,
+        init_image,
         negative_prompt,
         lora
     )
@@ -101,29 +106,47 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Music Video Generator")
     parser.add_argument(
-        "--input_video",
+        "--input-video",
         type=str,
         required=True,
         help="Input video path"
     )
     parser.add_argument(
-        "--output_folder",
+        "--output-folder",
         type=str,
         required=True,
         help="Output folder to save the generated images"
     )
     parser.add_argument(
-        "--model_id",
+        "--model-id",
         type=str,
         default="sd-legacy/stable-diffusion-v1-5",
         help="Model ID for Stable Diffusion"
     )
-    parser.add_argument("--prompt", type=str, required=True,
-                        help="Prompt for image generation")
-    parser.add_argument("--negative_prompt", type=str, required=False,
-                        help="Negative prompt for image generation")
-    parser.add_argument("--lora", type=str, required=False,
-                        help="Lora for image generation")
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        required=True,
+        help="Prompt for image generation"
+    )
+    parser.add_argument(
+        "--init-image",
+        type=str,
+        required=True,
+        help="First image for image generation"
+    )
+    parser.add_argument(
+        "--negative-prompt",
+        type=str,
+        required=False,
+        help="Negative prompt for image generation"
+    )
+    parser.add_argument(
+        "--lora",
+        type=str,
+        required=False,
+        help="Lora for image generation"
+    )
 
     args = parser.parse_args()
 
@@ -132,6 +155,7 @@ if __name__ == "__main__":
         args.output_folder,
         args.model_id,
         args.prompt,
+        args.init_image,
         args.negative_prompt,
         args.lora
     )
